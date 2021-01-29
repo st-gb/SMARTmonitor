c3fff8cf65566dfcf40cb67d1f858d265e08a9d9
Stefan Gebauer <hypoaktiv@yahoo.de>
2021-01-29 21:04:27 +0100

    moves files;etc.

--- src/createOSmenuItem.cmake
+++ src/createOSmenuItem.cmake
@@ -3,9 +3,13 @@
 
 if(UNIX)
   message("Calling shell script from CMake: " ${EXE_NAME})
+  set(cmdLine "../Linux/createOSmenuItemFile.sh \
+/usr/${exeInstallDir}/${EXE_NAME} \
+${resourcesFSpath}/icons/S.M.A.R.T._OK.xpm")
+  message("command line to exec:" ${cmdLine})
   #https://stackoverflow.com/questions/35072473/executing-bash-commands-from-a-cmake-file/42712713
   execute_process(
-    COMMAND bash -c "../Linux/createOSmenuItemFile.sh /usr/${exeInstallDir}/${EXE_NAME} ${resourcesFSpath}/icons/SMARTmonitor.desktop"
+    COMMAND bash -c ${cmdLine}
     OUTPUT_VARIABLE outVar
     )
 endif()
