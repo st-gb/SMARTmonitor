b40156537fce3b015d36e986c78c08fb0e25df92
Stefan Gebauer <s.gebauer@it-gebauer.de>
2021-06-27 16:20:04 +0200

    wxGUI:start,create Linux Release

--- start_wxSMARTmonitorGUI_as_root.sh
+++ start_wxSMARTmonitorGUI_as_root.sh
@@ -2,4 +2,18 @@
 BASEDIR=$(dirname $0)
 echo $BASEDIR
 cd $BASEDIR
-sudo ./wxSMARTmonitor_GUI.elf
+
+#https://unix.stackexchange.com/questions/199891/invalid-mit-magic-cookie-1-key-when-trying-to-run-program-remotely
+xhost +local: 
+
+numCmdLineArgs=$#
+
+if [ $numCmdLineArgs -lt 1 ]
+then
+  path=./wxSMARTmonitor_GUI_Debug.elf
+  echo "No argument passed->using default program name \""$path"\""
+else
+  path=$1
+  echo "using passed program name \""$path"\""
+fi
+sudo $path
